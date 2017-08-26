64fbf86d6b05804996cc488d5c360d56769f32d5
wiland <wiland2315@gmail.com>
2017-08-26 11:31:17 -0500

    Update files used in class

--- TypeScript ES6/app.js
+++ TypeScript ES6/app.js
@@ -1,7 +1,111 @@
-function saludar(nombre) {
-    console.log("Hola" + nombre);
-}
-var wolverine = {
-    nombre: "Logan"
-};
-saludar(wolverine.nombre);
+"use strict";
+// Interfaces
+// Promesas
+// let prom = new Promise(function(resolve, reject){
+//   setTimeout( () => {
+//     console.log("Promesa terminada");
+//     // SI todo sale bien
+//     // resolve();
+//
+//     //SI todo sale mal
+//     reject();
+//   }, 1000)
+// });
+//
+// console.log("Paso1");
+//
+// prom.then( () => {
+//   console.log("Salió bien");
+// }, () => {
+//   console.error("Salió mal");
+// })
+// Destructuración de arreglos
+// let avengers:string[] = ["Thor","Hulk","IronMan"];
+//
+// let [thor, bruce, tony] = avengers;
+// let [,, tony2] = avengers;
+// console.log(thor, bruce, tony);
+// console.log(tony);
+// Destructuración de objetos
+// let avenger = {
+//   nombre: "Tony Stark",
+//   clave: "Iron Man",
+//   poder: "Traje inteligente"
+// }
+//
+// let {nombre, clave, poder} = avenger;
+//
+// console.log(nombre, clave, poder);
+// Funciones de flecha
+// let superman = {
+//   nombre: "Clark",
+//   toString(){
+//     console.log("El nombre de superman es " + this.nombre);
+//   },
+//   toStringLentoF(){
+//     setTimeout(()=>console.log("El nombre de superman es " + this.nombre),1000)
+//   },
+//   toStringLento(){
+//     setTimeout(function(){
+//       console.log("El nombre de superman es " + this.nombre);
+//     }, 1000)
+//   }
+// }
+//
+// superman.toString();
+// superman.toStringLento();
+// superman.toStringLentoF();
+//
+// let funcion = function(a){
+//   return a;
+// }
+//
+// let funcionF = a => a;
+//
+// console.log(funcion("normal"));
+// console.log(funcionF("flecha"));
+//
+// let funcion2 = function(a:number, b:number){
+//   return a + b;
+// }
+//
+// let funcion2F = (a:number,b:number)=>a+b;
+//
+// console.log(funcion2(1,4));
+// console.log(funcion2F(6,8));
+//
+// let funcion3F = (nombre:string) => {
+//   nombre = nombre.toUpperCase();
+//   return nombre;
+// }
+//
+// console.log(funcion3F("Wilmer"));
+// Parámetros obligatorios
+// function activar( quien:string, objeto:string = "alarma", momento?:string){
+//   let mensaje:string;
+//   if (momento) {
+//     console.log(`${quien} activó la ${objeto} en la ${momento}`);
+//   } else {
+//     console.log(`${quien} activó la ${objeto}`);
+//   }
+// }
+//
+// activar("Andrés", "alarma", "noche");
+// Literales
+// let nombre = "Wilmer";
+// let apellido = "Córdoba";
+// let edad =  32;
+//
+// let texto = `Hola ${nombre} ${apellido} (${edad})`;
+//
+// console.log(texto);
+// Primera sesión Introductoria Typescript
+// function saludar( nombre:string ){
+//   console.log("Hola " + nombre.toUpperCase());
+// }
+//
+// var wolverine = {
+//   nombre: "Logan"
+// };
+//
+// saludar(wolverine.nombre);
