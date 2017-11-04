ae4b4711e74143e83de6b00bd5eb68918dfda6d9
wiland <wiland2315@gmail.com>
2017-11-04 17:25:54 -0500

    Actualización de curso

--- Proyectos/3.Pipes/src/app/app.component.ts
+++ Proyectos/3.Pipes/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = 'Wilmer';
+  nombreRaro = 'eL uSuarIo EsciBe uN pOCo raRO';
+  arreglo = [1,2,3,4,5,6,7];
+  PI = Math.PI;
+  perc = 0.3556;
+
+  salario = 1500000.5;
+
+  heroe = {
+    clave: 'Wolverine',
+    nombre: 'Logan',
+    poder: 'Adamantium'
+  };
+
+  valorDePromesa = new Promise((resolve,reject)=>{
+    setTimeout( ()=> resolve('Acabó'), 3000 );
+  });
+
+  fecha = new Date();
+
+  video = "Rqoq5oMOTaU";
 }
